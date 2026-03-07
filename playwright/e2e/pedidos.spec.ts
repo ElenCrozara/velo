94a563bfe98335bb77a037059891a6bd8b9e9a91
elen <elen.crozzara@gmail.com>
2026-03-07 07:19:46 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test'
+/// AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+    // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
+    // Act
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
   await page.getByTestId('search-order-id').fill('VLO-IAMOTK')
-
   await page.getByTestId('search-order-button').click()
+    // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-IAMOTK')
   await expect(page.getByTestId('order-result-status')).toBeVisible()
